96d656428d894b17b1fa4084447381f2d9d37862
sasi5868 <sasidharanaa@gmail.com>
2021-03-17 02:50:37 +0530

    emoji animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SelectMultipleControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'emoji-angular';
+
+  // sentence:string[];
+  isReading = false;
+  sentence: string[] = ["ai","l","au","uu"];
+  word;
+  phone = null;
+
+  async play() {
+    for(var i=0; i < this.sentence.length; i++) {
+      await this.delay(300);
+      this.isReading = true;
+      this.phone = this.sentence[i];
+      console.log("sentence "+i+" is : " +this.sentence[i]);
+      this.word = this.sentence[i];
+    }
+    this.isReading = !this.isReading;
+    this.phone = null;
+
+  }
+
+  private delay(ms: number)
+  {
+    console.log("inside delay function");
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
